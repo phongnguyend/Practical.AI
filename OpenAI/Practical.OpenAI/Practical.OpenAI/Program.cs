a28c9e7dd713f2b9671be4f417b4e07b269f05d7
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-04-09 09:03:49 +0700

    Simple Chat Bot with OpenAI

---
+++ OpenAI/Practical.OpenAI/Practical.OpenAI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
+
+var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
+var configuration = builder.Build();
+
+var services = new ServiceCollection();
+
+string openAiKey = configuration["OpenAI:GitHubToken"] ?? throw new Exception("Missing API Key");
+
+var openAIOptions = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.inference.ai.azure.com")
+};
+
+ChatClient client = new ChatClient("gpt-4o", new ApiKeyCredential(openAiKey), openAIOptions);
+
+var serviceProvider = services.BuildServiceProvider();
+
+var messages = new List<ChatMessage>
+{
+    new SystemChatMessage("You are a helpful assistant.")
+};
+
+while (true)
+{
+    Console.Write("You >> ");
+    var userInput = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(userInput))
+        break;
+
+    messages.Add(new UserChatMessage(userInput));
+    ChatCompletion response = await client.CompleteChatAsync(messages);
+    Console.WriteLine($"AI >> {response.Content[0].Text}");
+    messages.Add(new AssistantChatMessage(response.Content[0].Text));
+}
