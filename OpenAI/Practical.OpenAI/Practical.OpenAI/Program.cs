99bfb47797f57b3e450f1accfdc8a36c4aeb9e9b
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-04-25 12:58:30 +0700

    OpenAI

--- OpenAI/Practical.OpenAI/Practical.OpenAI/Program.cs
+++ OpenAI/Practical.OpenAI/Practical.OpenAI/Program.cs
@@ -20,21 +20,104 @@ ChatClient client = new ChatClient("gpt-4o", new ApiKeyCredential(openAiKey), op
 
 var serviceProvider = services.BuildServiceProvider();
 
-var messages = new List<ChatMessage>
+//await AskMappings(client, ["Company Number", "Email Address", "Ngày Giao Dịch"], ["Company Code", "Transaction Date"]);
+await AskColumns(client, "merge columns A and D to the column C", ["A", "B", "C", "D"]);
+
+//await NormalChat(client);
+
+static async Task AskMappings(ChatClient client, string[] headersInFile, string[] columnsInTable)
 {
-    new SystemChatMessage("You are a helpful assistant.")
-};
+    var messages = new List<ChatMessage>
+    {
+        new SystemChatMessage("You are a helpful assistant who is very good at mapping the column headers in excel with column names in a table in database."),
+        new UserChatMessage($"Here is the list of column headers in my file: {string.Join(',',headersInFile)}"),
+        new UserChatMessage($"And here is the list of columns in my table in database: {string.Join(',',columnsInTable)}"),
+        new UserChatMessage("Please try to best map the columns in file with columns in table if they are similar in the same or different languages and return result in json format"),
+        new UserChatMessage("The json structure should be [{file: 'Column In File', table: 'Column in Table', reason: 'the reason why you choose'}] and 'Column in Table' should be null if you cannot match")
+    };
 
-while (true)
-{
-    Console.Write("You >> ");
-    var userInput = Console.ReadLine();
-
-    if (string.IsNullOrEmpty(userInput))
-        break;
-
-    messages.Add(new UserChatMessage(userInput));
     ChatCompletion response = await client.CompleteChatAsync(messages);
-    Console.WriteLine($"AI >> {response.Content[0].Text}");
-    messages.Add(new AssistantChatMessage(response.Content[0].Text));
+    var reponseText = response.Content[0].Text;
+    messages.Add(new AssistantChatMessage(reponseText));
+
+    Console.WriteLine($"AI >> {reponseText}");
+
+    while (true)
+    {
+        Console.Write("You >> ");
+        var userInput = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(userInput))
+            break;
+
+        messages.Add(new UserChatMessage(userInput));
+
+        response = await client.CompleteChatAsync(messages);
+        reponseText = response.Content[0].Text;
+        messages.Add(new AssistantChatMessage(reponseText));
+
+        Console.WriteLine($"AI >> {reponseText}");
+    }
+}
+
+
+static async Task AskColumns(ChatClient client, string userRequest, string[] columns)
+{
+    string prompt = @$"here is a list of columns in a table in the database: {string.Join(',', columns)}
+this is a user request: {userRequest}
+please find out which columns we need to use to fulfill user's requirement";
+
+    var messages = new List<ChatMessage>
+    {
+        new SystemChatMessage("You are a helpful assistant who is very good at mapping the column headers in excel with column names in a table in database."),
+        new UserChatMessage(prompt)
+    };
+
+    ChatCompletion response = await client.CompleteChatAsync(messages);
+    var reponseText = response.Content[0].Text;
+    messages.Add(new AssistantChatMessage(reponseText));
+
+    Console.WriteLine($"AI >> {reponseText}");
+
+    while (true)
+    {
+        Console.Write("You >> ");
+        var userInput = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(userInput))
+            break;
+
+        messages.Add(new UserChatMessage(userInput));
+
+        response = await client.CompleteChatAsync(messages);
+        reponseText = response.Content[0].Text;
+        messages.Add(new AssistantChatMessage(reponseText));
+
+        Console.WriteLine($"AI >> {reponseText}");
+    }
+}
+
+static async Task NormalChat(ChatClient client)
+{
+    var messages = new List<ChatMessage>
+    {
+        new SystemChatMessage("You are a helpful assistant.")
+    };
+
+    while (true)
+    {
+        Console.Write("You >> ");
+        var userInput = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(userInput))
+            break;
+
+        messages.Add(new UserChatMessage(userInput));
+
+        ChatCompletion response = await client.CompleteChatAsync(messages);
+        var reponseText = response.Content[0].Text;
+        messages.Add(new AssistantChatMessage(reponseText));
+
+        Console.WriteLine($"AI >> {reponseText}");
+    }
 }
