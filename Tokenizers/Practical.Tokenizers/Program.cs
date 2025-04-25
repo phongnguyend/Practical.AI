c8307fee88af8580f8cec0af60ce888485b4e323
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-04-25 09:05:43 +0700

    Tokenizers

---
+++ Tokenizers/Practical.Tokenizers/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Tokenizers;
+
+Tokenizer tokenizer = TiktokenTokenizer.CreateForModel("gpt-4o");
+
+var input = "Chào anh em .Net Hội";
+
+var count = tokenizer.CountTokens(input);
+
+Console.WriteLine($"Number of Tokens: {count}");
+
+var tokens = tokenizer.EncodeToTokens(input, out string? normalizedText);
+
+foreach (var token in tokens)
+{
+    Console.WriteLine($"[{token.Id}, ({token.Offset.Start}, {token.Offset.End}), '{token.Value}']");
+}
