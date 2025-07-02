252824b9081e02dab2a106fe8b867c82178106ae
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-07-02 17:07:18 +0700

    Amazon Bedrock

---
+++ AmazonBedrock/Practical.AmazonBedrock/Practical.AmazonBedrock/Program.cs
@@ -0,0 +1,58 @@
+﻿using Amazon;
+using Amazon.BedrockRuntime;
+using Amazon.BedrockRuntime.Model;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
+var configuration = builder.Build();
+
+string awsAccessKeyId = configuration["Amazon:AccessKeyId"] ?? throw new Exception("Missing AccessKeyId");
+string awsSecretAccessKey = configuration["Amazon:SecretAccessKey"] ?? throw new Exception("Missing SecretAccessKey");
+string awsSessionToken = configuration["Amazon:SessionToken"] ?? throw new Exception("Missing SessionToken");
+
+// Create a Bedrock Runtime client in the AWS Region you want to use.
+var client = new AmazonBedrockRuntimeClient(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, RegionEndpoint.EUCentral1);
+
+// Define the user message.
+var userMessage = File.ReadAllText("../../prompt.txt");
+
+//Format the request payload using the model's native structure.
+var nativeRequest = JsonSerializer.Serialize(new
+{
+    anthropic_version = "bedrock-2023-05-31",
+    max_tokens = 512,
+    temperature = 0.5,
+    messages = new[]
+    {
+        new { role = "user", content = userMessage }
+    }
+});
+
+// Create a request with the model ID, the user message, and an inference configuration.
+var request = new InvokeModelRequest()
+{
+    ModelId = "arn:aws:bedrock:eu-central-1:891377219642:inference-profile/eu.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    Body = new MemoryStream(Encoding.UTF8.GetBytes(nativeRequest)),
+    ContentType = "application/json"
+};
+
+try
+{
+    // Send the request to the Bedrock Runtime and wait for the response.
+    var response = await client.InvokeModelAsync(request);
+
+    // Decode the response body.
+    var modelResponse = await JsonNode.ParseAsync(response.Body);
+
+    // Extract and print the response text.
+    var responseText = modelResponse["content"]?[0]?["text"] ?? "";
+    Console.WriteLine(responseText);
+}
+catch (AmazonBedrockRuntimeException e)
+{
+    Console.WriteLine($"ERROR: Can't invoke '{request.ModelId}'. Reason: {e.Message}");
+    throw;
+}
