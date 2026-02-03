f2e3d5b507dfe965ac8b2946295888a8b76b0cfc
Phong Nguyen <nguyendoanphong.it@gmail.com>
2026-02-03 18:09:19 +0700

    (#10) Azure AI Content Safety

---
+++ Azure AI Content Safety/Practical.AzureAIContentSafety/Practical.AzureAIContentSafety/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.AI.ContentSafety;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>();
+
+var configuration = builder.Build();
+
+string key = configuration["AzureAIContentSafety:ApiKey"]!;
+string endpoint = configuration["AzureAIContentSafety:Endpoint"]!;
+
+ContentSafetyClient client = new ContentSafetyClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+string text = "You are an idiot";
+
+var request = new AnalyzeTextOptions(text);
+
+Response<AnalyzeTextResult> response;
+try
+{
+    response = client.AnalyzeText(request);
+}
+catch (RequestFailedException ex)
+{
+    Console.WriteLine("Analyze text failed.\nStatus code: {0}, Error code: {1}, Error message: {2}", ex.Status, ex.ErrorCode, ex.Message);
+    throw;
+}
+
+Console.WriteLine("Hate severity: {0}", response.Value.CategoriesAnalysis.FirstOrDefault(a => a.Category == TextCategory.Hate)?.Severity ?? 0);
+Console.WriteLine("SelfHarm severity: {0}", response.Value.CategoriesAnalysis.FirstOrDefault(a => a.Category == TextCategory.SelfHarm)?.Severity ?? 0);
+Console.WriteLine("Sexual severity: {0}", response.Value.CategoriesAnalysis.FirstOrDefault(a => a.Category == TextCategory.Sexual)?.Severity ?? 0);
+Console.WriteLine("Violence severity: {0}", response.Value.CategoriesAnalysis.FirstOrDefault(a => a.Category == TextCategory.Violence)?.Severity ?? 0);
